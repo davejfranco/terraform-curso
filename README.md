e6855cd8dda0cf72ecdaa5e5e5e40f59d991a360
Dave Franco <davefranco1987@gmail.com>
2019-01-05 12:15:47 -0300

    first steps

---
+++ README.md
@@ -0,0 +1,5 @@
+# Terraform - Oracle Cloud Infrastructure Course
+
+
+Este repo esta destinado a un curso sobre como utilizar terraform enfocado a la nube de Oracle.
+
---
+++ variables.tf
@@ -0,0 +1,9 @@
+#Variable file
+
+#Auth
+variable "tenancy_ocid" {}
+variable "user_ocid" {}
+variable "fingerprint" {}
+variable "private_key_path" {}
+variable "region" {}
+
