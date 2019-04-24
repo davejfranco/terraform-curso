e644b6f6360e01bf32314bd22534bd38dfe27e5a
Dave Franco <davefranco1987@gmail.com>
2019-04-24 15:10:06 -0400

    add private subnets on outputs

--- modules/network/outputs.tf
+++ modules/network/outputs.tf
@@ -18,6 +18,10 @@ output "pub_route_table_id" {
   value = "${oci_core_route_table.net_pub_route_table.id}"
 }
 
+output "private_subnets" {
+  value = ["${oci_core_subnet.net_priv_subnets.*.id}"]
+}
+
 output "priv_route_table_id" {
   value = "${oci_core_route_table.net_priv_route_table.id}"
 }
