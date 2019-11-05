eec4c1da7ddf9d50da69c8ec4c53498d83f689d4
Dave Franco <davefranco1987@gmail.com>
2019-11-05 11:18:18 -0300

    remove main compartment

--- main.tf
+++ main.tf
@@ -1,4 +1,6 @@
 #Connection to Oracle cloud
+# 
+
 provider "oci" {
   version      = ">= 3.0.0"
   tenancy_ocid = "${var.tenancy_ocid}"
@@ -8,33 +10,24 @@ provider "oci" {
   region = "${var.region}"
 }
 
-resource "oci_identity_compartment" "main" {
-    #Required
-    compartment_id = "${var.tenancy_ocid}"
-    description = "Main compartment of terraform demo"
-    name = "terraform_demo"
-
-}
-
 module "network" {
   source = "modules/network"
 
   env = "dev"
 
   tenancy_ocid = "${var.tenancy_ocid}"
-  compartment_id = "${oci_identity_compartment.main.id}"
+  compartment_id = "${var.dave_compartment_id}"
   compartment_name = "terraform_net_demo"
   vcn_cidr_block = "${var.vcn_cidr_block}"
   vcn_display_name = "${var.vcn_display_name}"
   vcn_pub_nets = "${var.vcn_pub_nets}"  
   vcn_priv_nets = "${var.vcn_priv_nets}"
 
-
 }
 
 resource "oci_identity_compartment" "oke_compartment" {
     #Required
-    compartment_id = "${oci_identity_compartment.main.id}"
+    compartment_id = "${var.dave_compartment_id}"
     description = "oke compartment"
     name = "k8s_clusters"
 
@@ -42,18 +35,19 @@ resource "oci_identity_compartment" "oke_compartment" {
     freeform_tags = {"Environment"= "${var.env}"}
 }
 
-module "oke" {
-  #source = "modules/oke"
-  source = "git@github.com:davejfranco/terraform-oke.git?ref=develop"
+# module "oke" {
+#   #source = "modules/oke"
+#   source = "git@github.com:davejfranco/terraform-oke.git?ref=develop"
 
-  compartment_id = "${oci_identity_compartment.oke_compartment.id}"
-  k8s_version = "v1.12.7"
-  cluster_name = "oke_demo_cluster"
-  vcn_id = "${module.network.vcn_id}"
-  service_lb_subnet_ids = ["${element(module.network.public_subnets, 0)}", "${element(module.network.public_subnets, 1)}"]
-  node_pool_name = "np_demo_1"
-  node_pool_subnet_ids = ["${element(module.network.public_subnets, 2)}"]
-  node_pool_environment = "dev"
-  node_pool_ssh_public_key = "${file("~/.ssh/id_rsa.pub")}"
+#   compartment_id = "${oci_identity_compartment.oke_compartment.id}"
+#   k8s_version = "v1.12.7"
+#   cluster_name = "oke_demo_cluster"
+#   vcn_id = "${module.network.vcn_id}"
+#   service_lb_subnet_ids = ["${element(module.network.public_subnets, 0)}", "${element(module.network.public_subnets, 1)}"]
+#   node_pool_name = "np_demo_1"
+#   node_pool_subnet_ids = ["${element(module.network.public_subnets, 2)}"]
+#   node_pool_environment = "dev"
+#   node_pool_ssh_public_key = "${file("~/.ssh/id_rsa.pub")}"
 
-}
+# }
+
