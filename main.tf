4c4ec155fb23353e74fa42dded78741eaea9ab3d
Dave Franco <davefranco1987@gmail.com>
2019-11-05 11:24:40 -0300

    commented code block

--- main.tf
+++ main.tf
@@ -35,19 +35,19 @@ resource "oci_identity_compartment" "oke_compartment" {
     freeform_tags = {"Environment"= "${var.env}"}
 }
 
-# module "oke" {
-#   #source = "modules/oke"
-#   source = "git@github.com:davejfranco/terraform-oke.git?ref=develop"
+module "oke" {
+  #source = "modules/oke"
+  source = "git@github.com:davejfranco/terraform-oke.git?ref=develop"
 
-#   compartment_id = "${oci_identity_compartment.oke_compartment.id}"
-#   k8s_version = "v1.12.7"
-#   cluster_name = "oke_demo_cluster"
-#   vcn_id = "${module.network.vcn_id}"
-#   service_lb_subnet_ids = ["${element(module.network.public_subnets, 0)}", "${element(module.network.public_subnets, 1)}"]
-#   node_pool_name = "np_demo_1"
-#   node_pool_subnet_ids = ["${element(module.network.public_subnets, 2)}"]
-#   node_pool_environment = "dev"
-#   node_pool_ssh_public_key = "${file("~/.ssh/id_rsa.pub")}"
+  compartment_id = "${oci_identity_compartment.oke_compartment.id}"
+  k8s_version = "v1.12.7"
+  cluster_name = "oke_demo_cluster"
+  vcn_id = "${module.network.vcn_id}"
+  service_lb_subnet_ids = ["${element(module.network.public_subnets, 0)}", "${element(module.network.public_subnets, 1)}"]
+  node_pool_name = "np_demo_1"
+  node_pool_subnet_ids = ["${element(module.network.public_subnets, 2)}"]
+  node_pool_environment = "dev"
+  node_pool_ssh_public_key = "${file("~/.ssh/id_rsa.pub")}"
 
-# }
+}
 
